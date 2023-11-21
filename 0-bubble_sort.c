8d4ce48023f36825043c77c3f38c0448f8d1fbdc
PinkNoteX <omarfarghaly8@gmail.com>
2023-11-21 11:42:20 -0800

    Task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int tmp = 0, x = 0, y = 0, swapped = 0;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (x = 0; x < size - 1; x++)
+	{
+		for (y = 0; y < size - x - 1; y++)
+		{
+			if (array[y] > array[y + 1])
+			{
+				tmp = array[y];
+				array[y] = array[y + 1];
+				array[y + 1] = tmp;
+				print_array(array, size);
+				swapped = 1;
+			}
+
+		}
+		if (swaaped == 0)
+			break;
+	}
+}
